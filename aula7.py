514197eb575ef0c0130f2927f490725a0a752fce
Lara Santana <larasantana6031@gmail.com>
2024-05-17 16:38:05 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,79 @@
+# nu = 10
+#  if nu == 2:
+#       print("ok")
+#  else:
+#       print("nao existe")
+
+#  nome = str(input("digite seu nome"))
+#  idade = int(input("digite sua idade"))
+#  curso = str(input("digite seu curso"))
+# number = int(input("digite um numero"))
+
+#  if number % 2 == 0:
+#      print("divisivel")
+#  elif number > 100:
+#      print("maior que 100")
+#  else:
+#       print("aqui nao tem nada")
+
+#  numero = 10 % 2 == 0
+#  print(numero)
+
+#1
+# laranja = int(input("digite um numero"))
+# if laranja == 0:
+#     print('esse numero é zero')
+# elif laranja > 0:
+#     print("esse numero é positivo")
+# else:
+#     print("esse numero é negativo")   
+
+# #2
+# limao = int(input("sua idade: "))
+# if limao >= 18:
+#     print('usuario ja pode ser preso') 
+# else:
+#     print("menor de idade!!") 
+
+# #3
+# gigante = int(input("escolha um numero"))
+# if gigante % 2 == 0:
+#     print("par")
+# else:
+#     print("impar")
+
+# #4
+# amora = int(input(">>"))
+# kiwi = int(input(">>"))
+# kaio = int(input(">>"))
+
+# if amora == kiwi == kaio:
+#    print("triangulo equilatero")
+# elif amora == kiwi and kaio != amora or kiwi == kaio:
+#    print("é isosceles")
+# else:
+#    print("é escaleno")  
+
+# #5
+# caju = int(input("digite um numero")) 
+# if caju % 5 == 0 and caju % 7 == 0:
+#     print("é multiplo de 5 e 7")
+# else:
+#     print("nao é nem multiplo de 5 e nem de 7")
+
+# #6
+# japones = int(input("digite um numero"))
+# if japones >=0 and japones >10:
+#     print("correto")
+# else:
+#     print("incorreto")    
+
+# #7 
+# pera = int(input("digite um numero")) 
+# if pera % 5 == 0:
+#     print("é multiplo de 5")
+# elif pera % 3 == 0:
+#     print("multiplo de 3")
+# else:
+#     print("nao é nem uma das opções")
+
